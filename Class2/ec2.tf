8c95b173a8d09a3ae8d9ce9b573facf588d47d47
azizbekmol <azizmdov@gmail.com>
2021-02-17 23:37:20 +0000

    added volume

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "web" {
-  ami           = "ami-02354e95b39ca8dec"
-  instance_type = "t3.micro"
-  key_name = aws_key_pair.deployer2.key_name
+  ami                         = "ami-02354e95b39ca8dec"
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.deployer2.key_name
   associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  availability_zone = "us-east-1a"
-  user_data = file("userdata.sh")  
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+  availability_zone           = "us-east-1a"
+  user_data                   = file("userdata.sh")
   tags = {
-      "Name" = "Aziz"
+    "Name" = "Aziz"
   }
 }
