301b1ab4f751bee83ccb6bb53cbb860d7fac10cb
azizbekmol <azizmdov@gmail.com>
2021-02-19 18:57:38 +0000

    updated

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -1,13 +1,12 @@
 resource "aws_instance" "web" {
-  ami           = "ami-02354e95b39ca8dec"
-  instance_type = "t3.micro"
-  key_name = aws_key_pair.deployer2.key_name
-  count = 2
+  ami                         = "ami-02354e95b39ca8dec"
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.deployer2.key_name
   associate_public_ip_address = true
-  availability_zone = "us-east-1a"
-  user_data =  file("userdata.sh")
-
+  availability_zone           = "us-east-1a"
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+  user_data                   = file("userdata.sh")
   tags = {
-      "Name" = "Zalkar"
+      "Name" = "Azizbek"
   }
 }
