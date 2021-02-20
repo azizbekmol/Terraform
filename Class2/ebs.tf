95fb63b0055436061acacadd3f6f806b57b702e5
azizbekmol <azizmdov@gmail.com>
2021-02-20 00:02:55 +0000

    added import

--- Class2/ebs.tf
+++ Class2/ebs.tf
@@ -9,3 +9,14 @@ resource "aws_volume_attachment" "ebs_att" {
   instance_id = aws_instance.web.id
 }
 
+resource "aws_ebs_volume" "example2" {
+  availability_zone = "us-east-1a"
+  size              = 100
+}
+
+resource "aws_volume_attachment" "ebs_att2" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example2.id
+  instance_id = aws_instance.web2.id
+}
+
